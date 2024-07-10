41f055ba30b9806878ec138036dcb5e87096cc66
Kay Lack <kay@kaylack.net>
2024-07-10 10:23:24 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function add_one(num) {
+  return num + 1;
+}
+
+if (add_one(2) == 3) {
+  console.log("It works!")
+} else {
+  console.log("It does not work!")
+}
---
+++ main.py
@@ -0,0 +1,7 @@
+def add_one(num):
+  return num + 1
+
+if add_one(3) == 4:
+  print("It worked!")
+else:
+  print("It didn't work!")
